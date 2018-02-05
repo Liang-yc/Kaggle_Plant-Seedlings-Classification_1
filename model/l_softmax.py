norm(weight) use wrong axis, fixed

model/l_softmax.py
@@ -51,7 +51,7 @@ def l_softmax_training(input, target, margin, lambda_decay, weight):
     # weight = tf.nn.l2_normalize(weight, dim=1, name='weight_l2_normalize')
     input_norm = tf.norm(input, ord=2, axis=1, keep_dims=True,
                          name='input_l2_norm')
-    weight_norm = tf.norm(weight, ord=2, axis=1, keep_dims=True,
+    weight_norm = tf.norm(weight, ord=2, axis=0, keep_dims=True,
                           name='weight_l2_norm')
 
     logits = tf.matmul(input, weight)
@@ -61,9 +61,9 @@ def l_softmax_training(input, target, margin, lambda_decay, weight):
     logits_target_indices = tf.cast(tf.transpose(tf.stack([batch_index, target])), tf.int32)
 
     logits_target = tf.gather_nd(logits, logits_target_indices)
-    weight_target_norm = tf.gather(weight_norm, target, axis=0)
+    weight_target_norm = tf.gather(weight_norm, target, axis=1)
 
-    norm_target_prod = weight_target_norm * input_norm
+    norm_target_prod = tf.transpose(weight_target_norm) * input_norm
     # cos_target: (batch_size,)
     # Multiple-Angle Formulas
     cos_target = tf.expand_dims(logits_target, axis=1) / (norm_target_prod + 1e-10)

train.py
@@ -12,6 +12,7 @@ import sys
 import argparse
 import numpy as np
 import tensorflow as tf
+from tensorflow.python import debug as tf_debug
 
 import model.build_model as build_model
 import gen_dataset.data_provider as data_provider
@@ -29,18 +30,20 @@ TRAIN_CONFIG = {
 NUM_CLASS = 12
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", help="", default=64)
-parser.add_argument("--num_epoch", help="", default=999)
+parser.add_argument("--batch_size", help="", default=64, type=int)
+parser.add_argument("--num_epoch", help="", default=999, type=int)
 parser.add_argument(
-    "--early_stopping_step", help="", default=50)
+    "--early_stopping_step", help="", default=50, type=int)
 parser.add_argument(
-    "--lambda_decay_init", help="", default=1000.0)
+    "--lambda_decay_init", help="", default=1000.0, type=float)
 parser.add_argument(
-    "--lambda_decay_steps", help="", default=1000)
+    "--lambda_decay_steps", help="", default=1000, type=int)
 parser.add_argument(
-    "--lambda_decay_rate", help="", default=0.6)
+    "--lambda_decay_rate", help="", default=0.6, type=float)
 parser.add_argument(
-    "--lambda_decay_min", help="", default=5.0)
+    "--lambda_decay_min", help="", default=5.0, type=float)
+parser.add_argument(
+    "--tfdbg", help="", default=False, type=bool)
 args = parser.parse_args()
 
 
@@ -96,6 +99,8 @@ def train():
     session_config.gpu_options.allow_growth = True
 
     with tf.Session(config=session_config) as session:
+        if args.tfdbg:
+            session = tf_debug.LocalCLIDebugWrapperSession(session)
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
 